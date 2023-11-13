1fc210a01d55c6ac3573c5c52fb65ef7157f379e
guillaume <corentin.glm@icloud.com>
2023-11-13 11:02:57 +0100

    Add competency buttons to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,18 @@
+// import button from bootstrap
+import Button from 'react-bootstrap/Button';
+
 export function Home() {
     return (
         <>
-            <h1>Login page</h1>
+            <div>
+                <div className="left">
+                    <h1>Compétences</h1>
+                </div>
+                <div className="login">
+                    <Button variant="primary" href="/rh">RH</Button>
+                    <Button variant="primary" href="/collaborateur">Collaborateur</Button>
+                </div>
+            </div>
         </>
     )
 }
