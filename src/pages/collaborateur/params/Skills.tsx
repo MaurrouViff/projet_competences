a43cba0064bb66329a9e4aa90231fabf0bf90fcf
Aurélien <rofldu08@gmail.com>
2023-11-14 15:05:36 +0100

    Ajout des labels pour les compétences et d'un select

--- src/pages/collaborateur/params/Skills.tsx
+++ src/pages/collaborateur/params/Skills.tsx
@@ -1,6 +1,7 @@
 import {Layout} from "../layout.tsx";
 import '../../../assets/css/menu.css'
 import '../../../assets/css/salarie.css'
+import '../../../assets/css/collabo.css'
 import {LayoutCollaborateur} from "./layout.tsx";
 
 export function SkillsCollaborateur() {
@@ -20,11 +21,52 @@ export function SkillsCollaborateur() {
                         fontWeight: "bold",
                         borderBottom: "1px solid #000"
                     }}>
-                        <h2>Compétences</h2>
-                    </div>
+                        <h2>Tirage par compétences</h2>
+                        <SelectCollaboSkills/>
 
+                    </div>
+                    <h2>Résultat de la recherche</h2>
+                    <div>
+                        <div style={{display: "flex", flexDirection: "column", gap: "8px", borderBottom: "1px solid #000", padding: "8px 30px", margin: "0"}}>
+                            <p style={{fontWeight: "400"}}>Compétence</p>
+                            <p style={{fontWeight: "700", fontSize: "20px"}}>Label compétence</p>
+                        </div>
+
+                        <div style={{display: "flex", flexDirection: "column", gap: "8px", borderBottom: "1px solid #000", padding: "8px 30px", margin: "0"}}>
+                            <p style={{fontWeight: "400"}}>Compétence</p>
+                            <p style={{fontWeight: "700", fontSize: "20px"}}>Label compétence</p>
+                        </div>
+
+                        <div style={{display: "flex", flexDirection: "column", gap: "8px", borderBottom: "1px solid #000", padding: "8px 30px", margin: "0"}}>
+                            <p style={{fontWeight: "400"}}>Compétence</p>
+                            <p style={{fontWeight: "700", fontSize: "20px"}}>Label compétence</p>
+                        </div>
+
+                        <div style={{display: "flex", flexDirection: "column", gap: "8px", borderBottom: "1px solid #000", padding: "8px 30px", margin: "0"}}>
+                            <p style={{fontWeight: "400"}}>Compétence</p>
+                            <p style={{fontWeight: "700", fontSize: "20px"}}>Label compétence</p>
+                        </div>
+
+
+                    </div>
                 </div>
             </Layout>
         </div>
     )
 }
+import Form from 'react-bootstrap/Form';
+
+function SelectCollaboSkills() {
+    return (
+        <>
+
+            <br />
+            <Form.Select className="select-collabo">
+                <option>Sélectionner votre compétences</option>
+            </Form.Select>
+
+        </>
+    );
+}
+
+export default SelectCollaboSkills;
