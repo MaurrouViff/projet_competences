3202fa54945de086c80a754bd64a0176d4cd09e7
Aymeric DE LANGE <aymericdelange80@gmail.com>
2023-11-20 17:17:06 +0100

    feat: read evaluations for the rh page

--- src/pages/rh/params/Evaluations.tsx
+++ src/pages/rh/params/Evaluations.tsx
@@ -2,12 +2,36 @@ import {Layout} from "../layout.tsx";
 import '../../../assets/css/menu.css'
 import '../../../assets/css/salarie.css'
 import {LayoutRH} from "./layout.tsx";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {Supprimer} from "./supprimer.tsx";
+import supabase from "../../../lib/supabaseClient.ts";
+
+interface Evaluation {
+    idevaluation: number;
+    remarque: string;
+    nom: string;
+    idsalarie: number;
+}
 
 
 export function Evaluations() {
     const [showModal, setShowModal] = useState(false);
+
+    const [evalutations, setEvaluations] = useState<Evaluation[] | null>(null);
+
+    useEffect(() => {
+        async function readEvaluation() {
+            let { data: evaluation, error } = await supabase
+                .from('evaluation')
+                .select('*')
+
+            if (error) {
+                console.log(error);
+            }
+            return setEvaluations(evaluation)
+        }
+        readEvaluation();
+    }, []);
 
     return (
         <div className="skills salarie">
@@ -29,30 +53,17 @@ export function Evaluations() {
                     </div>
 
                     <div>
-                        <div
-                            style={{display: "flex", flexDirection: "row", gap: "8px", borderBottom: "1px solid #000", padding: "8px 30px", margin: "0"}}>
-                            <p style={{fontWeight: "700", fontSize: "20px", flex: "1"}}>Label évaluation</p>
-                            <button
-                                style={{backgroundColor: "#FFF", border: "1px solid #FF0000", borderRadius: "4px", padding: "8px 16px", color: "#FF0000"}}
-                                onClick={() => setShowModal(true)}>Supprimer
-                            </button>
-                        </div>
-                        <div
-                            style={{display: "flex", flexDirection: "row", gap: "8px", borderBottom: "1px solid #000", padding: "8px 30px", margin: "0"}}>
-                            <p style={{fontWeight: "700", fontSize: "20px", flex: "1"}}>Label évaluation</p>
-                            <button
-                                style={{backgroundColor: "#FFF", border: "1px solid #FF0000", borderRadius: "4px", padding: "8px 16px", color: "#FF0000"}}
-                                onClick={() => setShowModal(true)}>Supprimer
-                            </button>
-                        </div>
-                        <div
-                            style={{display: "flex", flexDirection: "row", gap: "8px", borderBottom: "1px solid #000", padding: "8px 30px", margin: "0"}}>
-                            <p style={{fontWeight: "700", fontSize: "20px", flex: "1"}}>Label évaluation</p>
-                            <button
-                                style={{backgroundColor: "#FFF", border: "1px solid #FF0000", borderRadius: "4px", padding: "8px 16px", color: "#FF0000"}}
-                                onClick={() => setShowModal(true)}>Supprimer
-                            </button>
-                        </div>
+                        {evalutations && evalutations.map(evaluation => (
+                            <div
+                                key={evaluation.idevaluation}
+                                style={{display: "flex", flexDirection: "row", gap: "8px", borderBottom: "1px solid #000", padding: "8px 30px", margin: "0"}}>
+                                <p style={{fontWeight: "700", fontSize: "20px", flex: "1"}}>{evaluation.nom}</p>
+                                <button
+                                    style={{backgroundColor: "#FFF", border: "1px solid #FF0000", borderRadius: "4px", padding: "8px 16px", color: "#FF0000"}}
+                                    onClick={() => setShowModal(true)}>Supprimer
+                                </button>
+                            </div>
+                        ))}
                     </div>
 
                     {showModal && <Supprimer setShowModal={setShowModal}/>}
