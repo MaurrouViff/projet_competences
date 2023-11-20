1544c870e6613d011bde0543b1639b75eeda84e8
Aymeric DE LANGE <aymericdelange80@gmail.com>
2023-11-20 09:33:27 +0100

    feat: read saliries from rh pages

--- src/pages/rh/params/Salarie.tsx
+++ src/pages/rh/params/Salarie.tsx
@@ -2,8 +2,32 @@ import {Layout} from "../layout.tsx";
 import '../../../assets/css/menu.css'
 import '../../../assets/css/salarie.css'
 import {LayoutRH} from "./layout.tsx";
+import {useEffect, useState} from "react";
+
+interface Salarie {
+    idsalarie: number;
+    nom: string;
+    prenom: string;
+}
+
+import supabase from "../../../lib/supabaseClient.ts";
 
 export function Salarie() {
+    const [salaries, setSalarie] = useState<Salarie[] | null>(null);
+
+    useEffect(() => {
+        async function readSalarie() {
+            let {data: salaries, error} = await supabase
+                .from('salarie')
+                .select('*')
+            if (error) {
+                console.log(error);
+            }
+            return setSalarie(salaries)
+        }
+        readSalarie();
+    }, []);
+
     return (
         <div className="salarie">
             <Layout>
@@ -24,27 +48,13 @@ export function Salarie() {
                     </div>
 
                     <div>
-                        <div style={{display: "flex", flexDirection: "column", gap: "8px", borderBottom: "1px solid #000", padding: "8px 30px", margin: "0"}}>
-                            <p style={{fontWeight: "400"}}>John Doe</p>
-                            <p style={{fontWeight: "700", fontSize: "20px"}}>Développeur mobile / IOS</p>
-                        </div>
-
-                        <div style={{display: "flex", flexDirection: "column", gap: "8px", borderBottom: "1px solid #000", padding: "8px 30px", margin: "0"}}>
-                            <p style={{fontWeight: "400"}}>John Doe</p>
-                            <p style={{fontWeight: "700", fontSize: "20px"}}>Développeur mobile / IOS</p>
-                        </div>
-
-                        <div style={{display: "flex", flexDirection: "column", gap: "8px", borderBottom: "1px solid #000", padding: "8px 30px", margin: "0"}}>
-                            <p style={{fontWeight: "400"}}>John Doe</p>
-                            <p style={{fontWeight: "700", fontSize: "20px"}}>Développeur mobile / IOS</p>
-                        </div>
-
-                        <div style={{display: "flex", flexDirection: "column", gap: "8px", borderBottom: "1px solid #000", padding: "8px 30px", margin: "0"}}>
-                            <p style={{fontWeight: "400"}}>John Doe</p>
-                            <p style={{fontWeight: "700", fontSize: "20px"}}>Développeur mobile / IOS</p>
-                        </div>
-
-
+                        {salaries && salaries.map((salarie) => (
+                            <div key={salarie.idsalarie}
+                                 style={{display: "flex", flexDirection: "column", gap: "8px", borderBottom: "1px solid #000", padding: "8px 30px", margin: "0"}}>
+                                <p style={{fontWeight: "400"}}>{salarie.prenom} {salarie.nom}</p>
+                                {/*<p style={{fontWeight: "700", fontSize: "20px"}}>Développeur mobile / IOS</p>*/}
+                            </div>
+                        ))}
                     </div>
                 </div>
             </Layout>
