5ee7d2feeba2baa8ed05752eeb4590847bcafdce
Aurélien <rofldu08@gmail.com>
2023-11-27 10:37:50 +0100

    Réparation de la page evaluation

--- src/pages/rh/params/Evaluations.tsx
+++ src/pages/rh/params/Evaluations.tsx
@@ -1,9 +1,9 @@
-import {Layout} from "../layout.tsx";
-import '../../../assets/css/menu.css'
-import '../../../assets/css/salarie.css'
-import {LayoutRH} from "./layout.tsx";
-import {useEffect, useState} from "react";
-import {Supprimer} from "./supprimer.tsx";
+import { Layout } from "../layout.tsx";
+import "../../../assets/css/menu.css";
+import "../../../assets/css/salarie.css";
+import { LayoutRH } from "./layout.tsx";
+import { useEffect, useState } from "react";
+import { Supprimer } from "./supprimer.tsx";
 import supabase from "../../../lib/supabaseClient.ts";
 
 interface Evaluation {
@@ -13,7 +13,6 @@ interface Evaluation {
     idsalarie: number;
 }
 
-
 export function Evaluations() {
     const [showModal, setShowModal] = useState(false);
 
@@ -22,8 +21,8 @@ export function Evaluations() {
     useEffect(() => {
         async function readEvaluation() {
             let { data: evaluation, error } = await supabase
-                .from('evaluation')
-                .select('*')
+                .from("evaluation")
+                .select("*");
 
             if (error) {
                 console.log(error);
@@ -33,44 +32,79 @@ export function Evaluations() {
         readEvaluation();
     }, []);
 
+    function renderEvaluation() {
+        if (evaluations && evaluations.length > 0) {
+            return evaluations?.map((evaluation) => (
+                <div
+                    key={evaluation.idevaluation}
+                    style={{
+                        display: "flex",
+                        flexDirection: "row",
+                        gap: "8px",
+                        borderBottom: "1px solid #000",
+                        padding: "8px 30px",
+                        margin: "0",
+                    }}
+                >
+                    <p style={{ fontWeight: "700", fontSize: "20px", flex: "1" }}>
+                        {evaluation.nom}
+                    </p>
+                    <button
+                        style={{
+                            backgroundColor: "#FFF",
+                            border: "1px solid #FF0000",
+                            borderRadius: "4px",
+                            padding: "8px 16px",
+                            color: "#FF0000",
+                        }}
+                        onClick={() => setShowModal(true)}
+                    >
+                        Supprimer
+                    </button>
+                </div>
+            ));
+        } else {
+            return (
+                <p className="loading text-center display-5 justify-content-center">
+                    Chargement...
+                </p>
+            );
+        }
+    }
+
     return (
         <div className="skills salarie">
             <Layout>
-                <LayoutRH/>
+                <LayoutRH />
 
-                <div style={{backgroundColor: "#FFF", width: "100%", overflowY: "auto", height: "100vh"}}>
-                    <div style={{
-                        height: "100px",
-                        width: "100%",
+                <div
+                    style={{
                         backgroundColor: "#FFF",
-                        display: "flex",
-                        alignItems: "center",
-                        paddingLeft: "30px",
-                        fontWeight: "bold",
-                        borderBottom: "1px solid #000"
-                    }}>
+                        width: "100%",
+                        overflowY: "auto",
+                        height: "100vh",
+                    }}
+                >
+                    <div
+                        style={{
+                            height: "100px",
+                            width: "100%",
+                            backgroundColor: "#FFF",
+                            display: "flex",
+                            alignItems: "center",
+                            paddingLeft: "30px",
+                            fontWeight: "bold",
+                            borderBottom: "1px solid #000",
+                        }}
+                    >
                         <h2>Evaluations</h2>
                     </div>
 
-                    <div>
-                        {evaluations && evaluations.map(evaluation => (
-                            <div
-                                key={evaluation.idevaluation}
-                                style={{display: "flex", flexDirection: "row", gap: "8px", borderBottom: "1px solid #000", padding: "8px 30px", margin: "0"}}>
-                                <p style={{fontWeight: "700", fontSize: "20px", flex: "1"}}>{evaluation.nom}</p>
-                                <button
-                                    style={{backgroundColor: "#FFF", border: "1px solid #FF0000", borderRadius: "4px", padding: "8px 16px", color: "#FF0000"}}
-                                    onClick={() => setShowModal(true)}>Supprimer
-                                </button>
+                    <div>{renderEvaluation()}</div>
 
-                            </div>
-                        ))}
-                    </div>
-
-                    {showModal && <Supprimer setShowModal={setShowModal}/>}
-
+                    {showModal && <Supprimer setShowModal={setShowModal} />}
                 </div>
             </Layout>
         </div>
-    )
+    );
 }
