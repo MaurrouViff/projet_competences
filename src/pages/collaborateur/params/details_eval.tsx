1848e4b5b12cf2c6ddbc45d0c0d4a86986603603
corentinglm <corentin.glm@icloud.com>
2023-11-29 13:42:08 +0100

    Modal Work in progress

--- src/pages/collaborateur/params/details_eval.tsx
+++ src/pages/collaborateur/params/details_eval.tsx
@@ -1,16 +1,68 @@
 // import {Modal} from "../../../components/Modal.tsx";
 import "../../../assets/css/modal.css";
 import { X } from "lucide-react";
+import { useState, useEffect } from "react";
+
+// import supabase
+import supabase from "../../../lib/supabaseClient.ts";
+import { Evaluations } from "../../rh/params/Evaluations.tsx";
 
 export function Details_Eval({
   setShowModal,
-  evalID
-  // parameters
-}: {
+  evalID,
+}: // parameters
+{
   setShowModal: (show: boolean) => void;
   evalID: string;
   // define type for parameters
 }) {
+  interface Evaluation {
+    idevaluation: number;
+    remarque: string;
+    nom: string;
+    idsalarie: number;
+  }
+
+  const [selectedEval, setSelectedEval] = useState<Evaluation | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function readEvaluation() {
+      let { data: evaluation, error } = await supabase
+        .from("evaluation")
+        .select("*")
+        .eq("idevaluation", evalID);
+
+      if (error) {
+        console.log(error);
+      } else if (evaluation && evaluation.length > 0) {
+        setSelectedEval(evaluation[0]);
+        setLoading(false);
+      } else {
+        setSelectedEval(null);
+      }
+    }
+
+
+    readEvaluation();
+  }, [evalID]);
+
+  if (loading) {
+    return (
+      <>
+        <div className="modal">
+          <div className="container">
+            <a onClick={() => setShowModal(false)}>
+              <X style={{ cursor: "pointer" }} />
+            </a>
+
+            <h2>Chargement</h2>
+          </div>
+        </div>
+      </>
+    );
+  }
+
   return (
     <>
       {/*<Modal/>*/}
@@ -20,7 +72,7 @@ export function Details_Eval({
             <X style={{ cursor: "pointer" }} />
           </a>
 
-          <h2>let's build from here ({evalID})</h2>
+          <h2>{selectedEval?.nom}</h2>
         </div>
       </div>
       {/*style={{width: "80vw", height: "80vh", backgroundColor: "#333", position: "absolute", bottom: 0, right: 0}}*/}
