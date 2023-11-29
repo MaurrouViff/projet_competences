62e3c4b2826fa1db977be01812dd6967c0907f32
Aurélien <rofldu08@gmail.com>
2023-11-29 15:24:57 +0100

    Ajout de la lecture des évaluations & compétences

--- src/pages/collaborateur/params/Evaluations.tsx
+++ src/pages/collaborateur/params/Evaluations.tsx
@@ -1,71 +1,127 @@
-import {Layout} from "../layout.tsx";
-import '../../../assets/css/menu.css'
-import '../../../assets/css/salarie.css'
-import '../../../assets/css/collabo.css'
-import {LayoutCollaborateur} from "./layout.tsx";
+import { Layout } from "../layout.tsx";
+import "../../../assets/css/menu.css";
+import "../../../assets/css/salarie.css";
+import "../../../assets/css/collabo.css";
+import { LayoutCollaborateur } from "./layout.tsx";
+import { useState, useEffect } from "react";
+import { X } from "lucide-react";
+import supabase from "../../../lib/supabaseClient.ts";
+
+interface Evaluation {
+    idevaluation: number;
+    remarque: string;
+    nom: string;
+    idsalarie: number;
+}
 
 export function EvaluationsCollaborateur() {
+    const [showModal, setShowModal] = useState(false);
+    const [evaluations, setEvaluations] = useState<Evaluation[] | null>(null);
+
+    useEffect(() => {
+        async function readEvaluation() {
+            try {
+                const { data: evaluation, error } = await supabase
+                    .from("evaluation")
+                    .select("*");
+
+                if (error) {
+                    console.error(error);
+                    return;
+                }
+                setEvaluations(evaluation);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        readEvaluation();
+    }, []);
+
+    function renderEvaluation() {
+        if (evaluations && evaluations.length > 0) {
+            return evaluations?.map((evaluation) => (
+                <div
+                    key={evaluation.idevaluation}
+                    style={{
+                        display: "flex",
+                        flexDirection: "row",
+                        gap: "8px",
+                        borderBottom: "1px solid #000",
+                        padding: "8px 30px",
+                        margin: "0",
+                    }}
+                >
+                    <p style={{ fontWeight: "700", fontSize: "20px", flex: "1" }}>
+                        {evaluation.nom}
+                    </p>
+
+                </div>
+            ));
+        } else {
+            return (
+                <p className="loading text-center display-5 justify-content-center">
+                    Chargement...
+                </p>
+            );
+        }
+    }
+
     return (
         <div className="skills salarie">
             <Layout>
-                <LayoutCollaborateur/>
+                <LayoutCollaborateur />
 
-                <div style={{backgroundColor: "#FFF", width: "100%", overflowY: "auto", height: "100vh"}}>
-                    <div style={{
-                        height: "100px",
-                        width: "100%",
+                <div
+                    style={{
                         backgroundColor: "#FFF",
-                        display: "flex",
-                        alignItems: "center",
-                        paddingLeft: "30px",
-                        fontWeight: "bold",
-                        borderBottom: "1px solid #000"
-                    }}>
+                        width: "100%",
+                        overflowY: "auto",
+                        height: "100vh",
+                    }}
+                >
+                    <div
+                        style={{
+                            height: "100px",
+                            width: "100%",
+                            backgroundColor: "#FFF",
+                            display: "flex",
+                            alignItems: "center",
+                            paddingLeft: "30px",
+                            fontWeight: "bold",
+                            borderBottom: "1px solid #000",
+                        }}
+                    >
                         <h2>Evaluations</h2>
-                        <SelectCollaboEval/>
-
                     </div>
-                    <div>
-                        <div style={{display: "flex", flexDirection: "column", gap: "8px", borderBottom: "1px solid #000", padding: "8px 30px", margin: "0"}}>
-                            <p style={{fontWeight: "400"}}>Nom évaluation</p>
-                            <p style={{fontWeight: "700", fontSize: "20px"}}>Label évaluation</p>
+
+                    <div>{renderEvaluation()}</div>
+
+                    {showModal && (
+                        <div className="modal">
+                            <div className="container">
+                                <a onClick={() => setShowModal(false)}>
+                                    <X style={{ cursor: "pointer" }} />
+                                </a>
+
+                                <h2>Contenu de la modal</h2>
+
+                                <button
+                                    style={{
+                                        backgroundColor: "#FFF",
+                                        border: "1px solid #1cff00",
+                                        borderRadius: "4px",
+                                        padding: "8px 16px",
+                                        color: "#1cff00",
+                                    }}
+                                    onClick={() => setShowModal(false)}
+                                >
+                                    Valider
+                                </button>
+                            </div>
                         </div>
-
-                        <div style={{display: "flex", flexDirection: "column", gap: "8px", borderBottom: "1px solid #000", padding: "8px 30px", margin: "0"}}>
-                            <p style={{fontWeight: "400"}}>Nom évaluation</p>
-                            <p style={{fontWeight: "700", fontSize: "20px"}}>Label évaluation</p>
-                        </div>
-
-                        <div style={{display: "flex", flexDirection: "column", gap: "8px", borderBottom: "1px solid #000", padding: "8px 30px", margin: "0"}}>
-                            <p style={{fontWeight: "400"}}>Nom évaluation</p>
-                            <p style={{fontWeight: "700", fontSize: "20px"}}>Label évaluation</p>
-                        </div>
-
-                        <div style={{display: "flex", flexDirection: "column", gap: "8px", borderBottom: "1px solid #000", padding: "8px 30px", margin: "0"}}>
-                            <p style={{fontWeight: "400"}}>Nom évaluation</p>
-                            <p style={{fontWeight: "700", fontSize: "20px"}}>Label évaluation</p>
-                        </div>
-
-
-                    </div>
+                    )}
                 </div>
             </Layout>
         </div>
-    )
-}
-import Form from 'react-bootstrap/Form';
-
-function SelectCollaboEval() {
-    return (
-        <>
-
-            <br />
-            <Form.Select className="select-collabo">
-                <option>Sélectionner votre évaluation</option>
-            </Form.Select>
-
-        </>
     );
 }
-
-export default SelectCollaboEval;
--- src/pages/collaborateur/params/Skills.tsx
+++ src/pages/collaborateur/params/Skills.tsx
@@ -3,56 +3,96 @@ import '../../../assets/css/menu.css'
 import '../../../assets/css/salarie.css'
 import '../../../assets/css/collabo.css'
 import {LayoutCollaborateur} from "./layout.tsx";
+import { useEffect, useState } from "react";
+import supabase from "../../../lib/supabaseClient.ts";
 
 export function SkillsCollaborateur() {
+    const [skills, setSkills] = useState(null);
+
+    useEffect(() => {
+        async function readSkills() {
+            try {
+                const { data: competence, error } = await supabase
+                    .from("comp")
+                    .select("*");
+
+                if (error) {
+                    console.error('Erreur lors du chargement des compétences depuis Supabase:', error);
+                    return;
+                }
+
+
+                setSkills(competence);
+            } catch (error) {
+                console.error('Erreur lors du chargement des compétences depuis Supabase:', error);
+            }
+        }
+
+
+        readSkills();
+    }, []);
+
+    function renderSkills() {
+        if (skills && skills.length > 0) {
+            return skills?.map((skill, index) => (
+                <div
+                    key={index}
+                    style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        gap: "8px",
+                        borderBottom: "1px solid #000",
+                        padding: "8px 30px",
+                        margin: "0",
+                    }}
+                >
+                    <p style={{ fontWeight: "400" }}>Compétence</p>
+                    <p style={{ fontWeight: "700", fontSize: "20px" }}>{skill.nom_competence}</p>
+                </div>
+            ));
+        } else {
+            return (
+                <p className="loading text-center display-5 justify-content-center">
+                    Chargement...
+                </p>
+            );
+        }
+    }
+
     return (
         <div className="skills salarie">
             <Layout>
-                <LayoutCollaborateur/>
+                <LayoutCollaborateur />
 
-                <div style={{backgroundColor: "#FFF", width: "100%", overflowY: "auto", height: "100vh"}}>
-                    <div style={{
-                        height: "100px",
-                        width: "100%",
+                <div
+                    style={{
                         backgroundColor: "#FFF",
-                        display: "flex",
-                        alignItems: "center",
-                        paddingLeft: "30px",
-                        fontWeight: "bold",
-                        borderBottom: "1px solid #000"
-                    }}>
+                        width: "100%",
+                        overflowY: "auto",
+                        height: "100vh",
+                    }}
+                >
+                    <div
+                        style={{
+                            height: "100px",
+                            width: "100%",
+                            backgroundColor: "#FFF",
+                            display: "flex",
+                            alignItems: "center",
+                            paddingLeft: "30px",
+                            fontWeight: "bold",
+                            borderBottom: "1px solid #000",
+                        }}
+                    >
                         <h2>Tirage par compétences</h2>
-                        <SelectCollaboSkills/>
-
+                        <SelectCollaboSkills />
                     </div>
                     <h2>Résultat de la recherche</h2>
-                    <div>
-                        <div style={{display: "flex", flexDirection: "column", gap: "8px", borderBottom: "1px solid #000", padding: "8px 30px", margin: "0"}}>
-                            <p style={{fontWeight: "400"}}>Compétence</p>
-                            <p style={{fontWeight: "700", fontSize: "20px"}}>Label compétence</p>
-                        </div>
-
-                        <div style={{display: "flex", flexDirection: "column", gap: "8px", borderBottom: "1px solid #000", padding: "8px 30px", margin: "0"}}>
-                            <p style={{fontWeight: "400"}}>Compétence</p>
-                            <p style={{fontWeight: "700", fontSize: "20px"}}>Label compétence</p>
-                        </div>
-
-                        <div style={{display: "flex", flexDirection: "column", gap: "8px", borderBottom: "1px solid #000", padding: "8px 30px", margin: "0"}}>
-                            <p style={{fontWeight: "400"}}>Compétence</p>
-                            <p style={{fontWeight: "700", fontSize: "20px"}}>Label compétence</p>
-                        </div>
-
-                        <div style={{display: "flex", flexDirection: "column", gap: "8px", borderBottom: "1px solid #000", padding: "8px 30px", margin: "0"}}>
-                            <p style={{fontWeight: "400"}}>Compétence</p>
-                            <p style={{fontWeight: "700", fontSize: "20px"}}>Label compétence</p>
-                        </div>
-
-
-                    </div>
+                    <div>{renderSkills()}</div>
                 </div>
             </Layout>
         </div>
-    )
+    );
 }
 import Form from 'react-bootstrap/Form';
 
