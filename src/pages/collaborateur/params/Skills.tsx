c81cc30c6f22d8718964af3a69953f097eb4bb40
corentinglm <corentin.glm@icloud.com>
2023-11-29 13:57:24 +0100

    update skills page on collab; working on rh page

--- src/pages/collaborateur/params/Skills.tsx
+++ src/pages/collaborateur/params/Skills.tsx
@@ -1,72 +1,120 @@
-import {Layout} from "../layout.tsx";
-import '../../../assets/css/menu.css'
-import '../../../assets/css/salarie.css'
-import '../../../assets/css/collabo.css'
-import {LayoutCollaborateur} from "./layout.tsx";
+import { Layout } from "../layout.tsx";
+import "../../../assets/css/menu.css";
+import "../../../assets/css/salarie.css";
+import "../../../assets/css/collabo.css";
+import { LayoutCollaborateur } from "./layout.tsx";
+
+import { useEffect, useState } from "react";
+
+// import supabase
+import supabase from "../../../lib/supabaseClient.ts";
 
 export function SkillsCollaborateur() {
-    return (
-        <div className="skills salarie">
-            <Layout>
-                <LayoutCollaborateur/>
+  interface Skills {
+    idcompetence: number;
+    titre: string;
+    nom_domaine: string;
+  }
 
-                <div style={{backgroundColor: "#FFF", width: "100%", overflowY: "auto", height: "100vh"}}>
-                    <div style={{
-                        height: "100px",
-                        width: "100%",
-                        backgroundColor: "#FFF",
-                        display: "flex",
-                        alignItems: "center",
-                        paddingLeft: "30px",
-                        fontWeight: "bold",
-                        borderBottom: "1px solid #000"
-                    }}>
-                        <h2>Tirage par compétences</h2>
-                        <SelectCollaboSkills/>
+  const [showModal, setShowModal] = useState(false);
 
-                    </div>
-                    <h2>Résultat de la recherche</h2>
-                    <div>
-                        <div style={{display: "flex", flexDirection: "column", gap: "8px", borderBottom: "1px solid #000", padding: "8px 30px", margin: "0"}}>
-                            <p style={{fontWeight: "400"}}>Compétence</p>
-                            <p style={{fontWeight: "700", fontSize: "20px"}}>Label compétence</p>
-                        </div>
+  // <Skills[] | null>
+  const [skills, setSkills] = useState<Skills[] | null>(null);
 
-                        <div style={{display: "flex", flexDirection: "column", gap: "8px", borderBottom: "1px solid #000", padding: "8px 30px", margin: "0"}}>
-                            <p style={{fontWeight: "400"}}>Compétence</p>
-                            <p style={{fontWeight: "700", fontSize: "20px"}}>Label compétence</p>
-                        </div>
+  useEffect(() => {
+    async function readEvaluation() {
+      let { data: skills, error } = await supabase
+        .from("comp")
+        .select("*");
 
-                        <div style={{display: "flex", flexDirection: "column", gap: "8px", borderBottom: "1px solid #000", padding: "8px 30px", margin: "0"}}>
-                            <p style={{fontWeight: "400"}}>Compétence</p>
-                            <p style={{fontWeight: "700", fontSize: "20px"}}>Label compétence</p>
-                        </div>
+      if (error) {
+        console.log(error);
+      }
+      return setSkills(skills);
+    }
+    readEvaluation();
+  }, []);
 
-                        <div style={{display: "flex", flexDirection: "column", gap: "8px", borderBottom: "1px solid #000", padding: "8px 30px", margin: "0"}}>
-                            <p style={{fontWeight: "400"}}>Compétence</p>
-                            <p style={{fontWeight: "700", fontSize: "20px"}}>Label compétence</p>
-                        </div>
-
-
-                    </div>
-                </div>
-            </Layout>
+  function renderSkills() {
+    if (skills && skills.length > 0) {
+      return skills?.map((skill, index) => (
+        <div
+          key={index}
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            gap: "8px",
+            borderBottom: "1px solid #000",
+            padding: "8px 30px",
+            margin: "0",
+          }}
+        >
+          <div className="div">
+            <p style={{ fontWeight: "600" }}>{skill.nom_domaine}</p>
+            <p style={{ fontWeight: "400" }}>{skill.nom_bloc}</p>
+            <p style={{ fontWeight: "700", fontSize: "20px" }}>
+              {skill.nom_competence}
+            </p>
+          </div>
         </div>
-    )
+      ));
+    } else {
+      return (
+        <p className="loading text-center display-5 justify-content-center">
+          Chargement...
+        </p>
+      );
+    }
+  }
+
+  return (
+    <div className="skills salarie">
+      <Layout>
+        <LayoutCollaborateur />
+
+        <div
+          style={{
+            backgroundColor: "#FFF",
+            width: "100%",
+            overflowY: "auto",
+            height: "100vh",
+          }}
+        >
+          <div
+            style={{
+              height: "100px",
+              width: "100%",
+              backgroundColor: "#FFF",
+              display: "flex",
+              alignItems: "center",
+              paddingLeft: "30px",
+              fontWeight: "bold",
+              borderBottom: "1px solid #000",
+            }}
+          >
+            <h2>Tirage par compétences</h2>
+            <SelectCollaboSkills />
+          </div>
+          <h2>Résultat de la recherche</h2>
+          <div>
+            <div>{renderSkills()}</div>
+          </div>
+        </div>
+      </Layout>
+    </div>
+  );
 }
-import Form from 'react-bootstrap/Form';
+import Form from "react-bootstrap/Form";
 
 function SelectCollaboSkills() {
-    return (
-        <>
-
-            <br />
-            <Form.Select className="select-collabo">
-                <option>Sélectionner votre compétences</option>
-            </Form.Select>
-
-        </>
-    );
+  return (
+    <>
+      <br />
+      <Form.Select className="select-collabo">
+        <option>Sélectionner votre compétences</option>
+      </Form.Select>
+    </>
+  );
 }
 
 export default SelectCollaboSkills;
