f93bdcb356e8920ae6352dbc12a622a4c70aecd3
corentinglm <corentin.glm@icloud.com>
2023-11-29 14:31:41 +0100

    Refonte des skills, update modal

--- src/pages/rh/params/Evaluations.tsx
+++ src/pages/rh/params/Evaluations.tsx
@@ -5,106 +5,105 @@ import { LayoutRH } from "./layout.tsx";
 import { useEffect, useState } from "react";
 import { Supprimer } from "./supprimer.tsx";
 import supabase from "../../../lib/supabaseClient.ts";
+import { Details_Eval } from "./details_eval.tsx";
 
 interface Evaluation {
-    idevaluation: number;
-    remarque: string;
-    nom: string;
-    idsalarie: number;
+  idevaluation: number;
+  remarque: string;
+  nom: string;
+  idsalarie: number;
 }
 
 export function Evaluations() {
-    const [showModal, setShowModal] = useState(false);
 
-    const [evaluations, setEvaluations] = useState<Evaluation[] | null>(null);
 
-    useEffect(() => {
-        async function readEvaluation() {
-            let { data: evaluation, error } = await supabase
-                .from("evaluation")
-                .select("*");
+  const [showModal, setShowModal] = useState(false);
+  const [showDetails, setShowDetails] = useState(false);
 
-            if (error) {
-                console.log(error);
-            }
-            return setEvaluations(evaluation);
-        }
-        readEvaluation();
-    }, []);
+  const [evaluations, setEvaluations] = useState<Evaluation[] | null>(null);
 
-    function renderEvaluation() {
-        if (evaluations && evaluations.length > 0) {
-            return evaluations?.map((evaluation) => (
-                <div
-                    key={evaluation.idevaluation}
-                    style={{
-                        display: "flex",
-                        flexDirection: "row",
-                        gap: "8px",
-                        borderBottom: "1px solid #000",
-                        padding: "8px 30px",
-                        margin: "0",
-                    }}
-                >
-                    <p style={{ fontWeight: "700", fontSize: "20px", flex: "1" }}>
-                        {evaluation.nom}
-                    </p>
-                    <button
-                        style={{
-                            backgroundColor: "#FFF",
-                            border: "1px solid #FF0000",
-                            borderRadius: "4px",
-                            padding: "8px 16px",
-                            color: "#FF0000",
-                        }}
-                        onClick={() => setShowModal(true)}
-                    >
-                        Supprimer
-                    </button>
-                </div>
-            ));
-        } else {
-            return (
-                <p className="loading text-center display-5 justify-content-center">
-                    Chargement...
-                </p>
-            );
-        }
+  const [selectedEval, setSelectedEval] = useState<number | null>(null);
+
+  useEffect(() => {
+    async function readEvaluation() {
+      let { data: evaluation, error } = await supabase
+        .from("evaluation")
+        .select("*");
+
+      if (error) {
+        console.log(error);
+      }
+      return setEvaluations(evaluation);
     }
+    readEvaluation();
+  }, []);
 
-    return (
-        <div className="skills salarie">
-            <Layout>
-                <LayoutRH />
-
-                <div
-                    style={{
-                        backgroundColor: "#FFF",
-                        width: "100%",
-                        overflowY: "auto",
-                        height: "100vh",
-                    }}
-                >
-                    <div
-                        style={{
-                            height: "100px",
-                            width: "100%",
-                            backgroundColor: "#FFF",
-                            display: "flex",
-                            alignItems: "center",
-                            paddingLeft: "30px",
-                            fontWeight: "bold",
-                            borderBottom: "1px solid #000",
-                        }}
-                    >
-                        <h2>Evaluations</h2>
-                    </div>
-
-                    <div>{renderEvaluation()}</div>
-
-                    {showModal && <Supprimer setShowModal={setShowModal} />}
-                </div>
-            </Layout>
+  function renderEvaluation() {
+    if (evaluations && evaluations.length > 0) {
+      return evaluations?.map((evaluation) => (
+        <div
+          onClick={() => {
+            setShowDetails(true);
+            setSelectedEval(evaluation.idevaluation);
+          }}
+          key={evaluation.idevaluation}
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            gap: "8px",
+            borderBottom: "1px solid #000",
+            padding: "8px 30px",
+            margin: "0",
+          }}
+        >
+          <p style={{ fontWeight: "700", fontSize: "20px", flex: "1" }}>
+            {evaluation.nom}
+          </p>
         </div>
-    );
+      ));
+    } else {
+      return (
+        <p className="loading text-center display-5 justify-content-center">
+          Chargement...
+        </p>
+      );
+    }
+  }
+
+  return (
+    <div className="skills salarie">
+      <Layout>
+        <LayoutRH />
+
+        <div
+          style={{
+            backgroundColor: "#FFF",
+            width: "100%",
+            overflowY: "auto",
+            height: "100vh",
+          }}
+        >
+          <div
+            style={{
+              height: "100px",
+              width: "100%",
+              backgroundColor: "#FFF",
+              display: "flex",
+              alignItems: "center",
+              paddingLeft: "30px",
+              fontWeight: "bold",
+              borderBottom: "1px solid #000",
+            }}
+          >
+            <h2>Evaluations</h2>
+          </div>
+
+          <div>{renderEvaluation()}</div>
+
+          {showModal && <Supprimer setShowModal={setShowModal} />}
+          {showDetails && <Details_Eval setShowModal={setShowDetails} evalID={selectedEval} />}
+        </div>
+      </Layout>
+    </div>
+  );
 }
